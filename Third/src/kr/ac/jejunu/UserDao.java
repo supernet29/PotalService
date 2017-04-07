e268b28dd4ff9441cfa1071a4665414ae65c20e9
WoochanKim <woochankim200@gmail.com>
2017-04-07 18:54:34 +0900

    chage code to use jdbcTemplate.

--- Third/src/kr/ac/jejunu/UserDao.java
+++ Third/src/kr/ac/jejunu/UserDao.java
@@ -1,45 +1,62 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.*;
-import java.util.Objects;
 
 /**
  * Created by super on 2017-03-15.
  */
 public class UserDao {
-
-    private DBContext context;
+    private JdbcTemplate context ;
 
     public User getUser(long id) throws SQLException, ClassNotFoundException {
         String sql = "select id, name, password from userinfo where id = ?";
         Object[] parms = new Object[] {id};
-        PrepareStatementStrategy statementStrategy = context.prepareStatementParmameterSetting(sql, parms);
-        return context.getQuery(statementStrategy);
+        User user1 = null;
+        try {
+            user1 = context.queryForObject(sql, parms, (resultSet, i) -> {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            });
+        } catch (org.springframework.dao.DataAccessException dataAccessException) {
+            dataAccessException.printStackTrace();
+        }
+        return user1;
     }
 
     public Long addUser(User user) throws ClassNotFoundException, SQLException {
         String sql = "insert into userinfo (name, password) values(?, ?)";
         Object[] parms = new Object[] {user.getName(), user.getPassword()};
-        PrepareStatementStrategy statementStrategy = context.prepareStatementParmameterSetting(sql, parms);
-        return context.addQuery(statementStrategy);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        context.update((connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getPassword());
+            return statement;
+        }),keyHolder);
+        return (Long) keyHolder.getKey();
     }
 
     public void updateUser(User user) throws SQLException, ClassNotFoundException {
         String sql = "update userinfo set name=?, password=? where id = ?";
         Object[] parms = new Object[]{user.getName(), user.getPassword(), user.getId()};
-        PrepareStatementStrategy statementStrategy = context.prepareStatementParmameterSetting(sql, parms);
-        context.updateQuery(statementStrategy);
+        context.update(sql, parms);
     }
 
     public void deleteUser(Long id) throws SQLException, ClassNotFoundException {
         String sql = "delete from userinfo where id = ?";
         Object[] parms = new Object[]{id};
-        PrepareStatementStrategy statementStrategy = context.prepareStatementParmameterSetting(sql, parms);
-        context.updateQuery(statementStrategy);
+        context.update(sql, parms);
     }
 
 
-    public void setContext(DBContext context) {
+    public void setContext(JdbcTemplate context) {
         this.context = context;
     }
 }
