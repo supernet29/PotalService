224f817dcc9405c5115ea2946006785e5f0164c5
WoochanKim <woochankim200@gmail.com>
2017-04-14 18:40:43 +0900

    there are too many one function class. callback pattern applied.

--- First/src/kr/ac/jejunu/UserDao.java
+++ First/src/kr/ac/jejunu/UserDao.java
@@ -10,18 +10,42 @@ public class UserDao {
     private DBContext dbContext;
 
     public User getUser(long id) throws SQLException, ClassNotFoundException {
-        PreparedStatementStrategy statementStrategy = new GetUserPrepareStatementStrategy(id);
+        PreparedStatementStrategy statementStrategy = new PreparedStatementStrategy() {
+            @Override
+            public PreparedStatement getPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement query;
+                query = connection.prepareStatement("select id, name, password from userinfo where id = ?");
+                query.setLong(1, id);
+                return query;
+            }
+        };
         return dbContext.selectQuery(statementStrategy);
     }
 
     public void addUser(User user) throws ClassNotFoundException, SQLException {
-        PreparedStatementStrategy preparedStatementStrategy = new AddUserPrepareStatementStrategy(user);
+        PreparedStatementStrategy preparedStatementStrategy = new PreparedStatementStrategy() {
+            @Override
+            public PreparedStatement getPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement query = connection.prepareStatement("insert into userinfo (id, name, password) values(?, ?, ?)");
+                query.setLong(1, user.getId());
+                query.setString(2, user.getName());
+                query.setString(3, user.getPassword());
+                return query;
+            }
+        };
         dbContext.UpdateQuery(preparedStatementStrategy);
     }
 
 
     public void deleteUser(long id) throws SQLException {
-        PreparedStatementStrategy preparedStatementStrategy = new DeleteUserPrepareStatementStrategy(id);
+        PreparedStatementStrategy preparedStatementStrategy = new PreparedStatementStrategy() {
+            @Override
+            public PreparedStatement getPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement query = connection.prepareStatement("delete from userinfo where id = ?");
+                query.setLong(1, id);
+                return query;
+            }
+        };
         dbContext.UpdateQuery(preparedStatementStrategy);
     }
 
