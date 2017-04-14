ed98d4896ff249c91b36c0afaf1132d87d664674
WoochanKim <woochankim200@gmail.com>
2017-04-14 18:04:10 +0900

    when exception occured, try to close streams.

--- First/src/kr/ac/jejunu/UserDao.java
+++ First/src/kr/ac/jejunu/UserDao.java
@@ -12,29 +12,68 @@ public class UserDao {
 
     public User getUser(long id) throws SQLException, ClassNotFoundException {
         Connection connection = connectionMaker.getConnection();
-        PreparedStatement query = connection.prepareStatement("select id, name, password from userinfo where id = ?");
-        query.setLong(1, id);
-        ResultSet result = query.executeQuery();
-        result.next();
+        User user = null;
+        ResultSet result = null;
+        PreparedStatement query = null;
+        try {
+            query = connection.prepareStatement("select id, name, password from userinfo where id = ?");
+            query.setLong(1, id);
+            result = query.executeQuery();
+            user = null;
+            if (result.next()) {
+                user = new User();
+                user.setId(result.getLong("id"));
+                user.setName(result.getString("name"));
+                user.setPassword(result.getString("password"));
+            }
+        } finally {
+            if(result != null)
+                try {
+                    result.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(query != null)
+                try {
+                    query.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
 
-        User user = new User();
-        user.setId(result.getLong("id"));
-        user.setName(result.getString("name"));
-        user.setPassword(result.getString("password"));
         return user;
     }
 
 
     public void addUser(User user) throws ClassNotFoundException, SQLException {
         Connection connection = connectionMaker.getConnection();
-        PreparedStatement query = connection.prepareStatement("insert into userinfo (id, name, password) values(?, ?, ?)");
-        query.setLong(1, user.getId());
-        query.setString(2, user.getName());
-        query.setString(3, user.getPassword());
-        query.executeUpdate();
-
-        query.close();
-        connection.close();
+        PreparedStatement query = null;
+        try {
+            query = connection.prepareStatement("insert into userinfo (id, name, password) values(?, ?, ?)");
+            query.setLong(1, user.getId());
+            query.setString(2, user.getName());
+            query.setString(3, user.getPassword());
+            query.executeUpdate();
+        } finally {
+            if(query != null)
+                try {
+                    query.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(query != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 
     public void setConnectionMaker(DataSource connectionMaker) {
